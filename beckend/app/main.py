f009a5d76c161ce3ca898c03a635582908f58752
SAMIR RICARDO <samirricardo22@gmail.com>
2025-12-05 23:01:37 -0300

    Refactor imports and clean up main.py

--- beckend/app/main.py
+++ beckend/app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from app.routes import users, actions
-from .database import iniciar_banco
 from fastapi.middleware.cors import CORSMiddleware
 
-
+from app.routes import users, actions
+from app.database import iniciar_banco
 
 app = FastAPI(title="ola")
 
@@ -13,29 +12,20 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ou ["http://localhost:4200"] se quiser restringir
+    allow_origins=["*"],  # ou origins se quiser restringir
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-
-
 @app.on_event("startup")
 async def startup_event():
     await iniciar_banco()
-    
+
 app.include_router(users.router)
 app.include_router(actions.router)
 
 
-
 @app.get("/")
-
-
 def read_root():
-
-    return{"mensagem" : "ola"}
-
-
+    return {"mensagem": "ola"}
