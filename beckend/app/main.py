7569a4b0da93017d35fe9e2ec16aafd72fd5d45d
SAMIR RICARDO <samirricardo22@gmail.com>
2025-12-08 14:26:03 -0300

    Refactor CORS configuration for origins

--- beckend/app/main.py
+++ beckend/app/main.py
@@ -7,20 +7,18 @@ from app.database import iniciar_banco
 app = FastAPI(title="Ecotrack API")
 
 # ✅ DOMÍNIOS PERMITIDOS
-origins = [
-    "http://localhost:4200",
-    "https://myecotrack.vercel.app",
-    "https://myecotrack-git-master-sr-samirs-projects.vercel.app",  # ← esse é o que aparece no erro
-]
-
-# ✅ CORS CONFIGURADO CORRETAMENTE
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://myecotrack.*\.vercel\.app",   # ⚠️ NÃO use "*" em produção com credentials=True
-    allow_credentials=True,
-    allow_methods=["*"],    # POST, GET, PUT, DELETE, OPTION
+    allow_origins=[
+        "https://myecotrack.vercel.app",
+        "https://myecotrack-git-master-sr-samirs-projects.vercel.app",
+        "http://localhost:4200",
+    ],
+    allow_credentials=True,   # se NÃO usar cookies, pode pôr False
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.on_event("startup")
 async def startup_event():
